e900505bccfbfff6964ea9484ec01e2bf33c82c5
Snorlax <maria.vallejo29@yahoo.com>
2023-01-05 23:09:22 -0600

    display new word at the touch of any button

--- main.py
+++ main.py
@@ -1,6 +1,18 @@
 from tkinter import *
+import pandas
+import random
 
 BACKGROUND_COLOR = "#B1DDC6"
+
+
+def random_word():
+    canvas.delete("word")
+    data = pandas.read_csv("./data/french_words.csv")
+    data_dict = data.to_dict(orient="records")
+    word = random.choice(data_dict)
+    foreign_word = word["French"]
+    front_word = canvas.create_text(400, 263, text=foreign_word, font=("Ariel", 40, "italic"), tags="word")
+
 
 window = Tk()
 window.title("Flashy")
@@ -11,15 +23,14 @@ card_img = PhotoImage(file="./images/card_front.png")
 canvas.create_image(400, 263, image=card_img)
 canvas.grid(column=0, row=0, columnspan=2)
 front_title = canvas.create_text(400, 150, text="French", font=("Ariel", 40, "italic"))
-front_word = canvas.create_text(400, 263, text="trouve", font=("Ariel", 40, "italic"))
+random_word()
 
 wrong_img = PhotoImage(file="./images/wrong.png")
-wrong_btn = Button(image=wrong_img, highlightthickness=0, bd=0)
+wrong_btn = Button(image=wrong_img, highlightthickness=0, bd=0, command=random_word)
 wrong_btn.grid(column=0, row=2)
 
 right_img = PhotoImage(file="./images/right.png")
-right_btn = Button(image=right_img, highlightthickness=0, bd=0 )
+right_btn = Button(image=right_img, highlightthickness=0, bd=0, command=random_word)
 right_btn.grid(column=1, row=2)
-
 
 window.mainloop()
